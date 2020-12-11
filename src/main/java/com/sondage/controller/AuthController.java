f9f6b3f588f320e02ebb34e749a70efd03879a66
athmaneHafsi <athmane95@hotmail.fr>
2020-12-11 20:15:32 +0100

    init Frontend(Angular), Ajout controlleur pour authentification

---
+++ src/main/java/com/sondage/controller/AuthController.java
@@ -0,0 +1,134 @@
+package com.sondage.controller;
+
+import com.sondage.DAO.impl.UserDetailsImpl;
+import com.sondage.config.JwtUtils;
+import com.sondage.model.*;
+import com.sondage.model.repository.RoleRepository;
+import com.sondage.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+
+    @PostMapping("/api/auth/inscription")
+    public ResponseEntity<?> inscription(@RequestBody SignupRequest signUpRequest){
+
+		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                .badRequest()
+                .body("Error: Username deja utilisé!");
+        }
+
+		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                .badRequest()
+                .body("Error: L'email est déja utilisée!");
+        }
+
+        signUpRequest.initialiseDateInscription();
+        signUpRequest.miseAJourDateConnection();
+
+        System.out.println(signUpRequest.getCivilite());
+
+    // Create new user's account
+    User user = new User(
+            signUpRequest.getUsername(),
+            signUpRequest.getEmail(),
+            encoder.encode(signUpRequest.getPassword()),
+            signUpRequest.getNom(),
+            signUpRequest.getPrenom(),
+            signUpRequest.getCivilite(),
+            signUpRequest.getDateInscription(),
+            signUpRequest.getDerniereConnexion()
+    );
+
+    Set<String> strRoles = signUpRequest.getRole();
+    Set<Role> roles = new HashSet<>();
+
+		if (strRoles == null) {
+        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        roles.add(userRole);
+    } else {
+        strRoles.forEach(role -> {
+            switch (role) {
+                case "admin":
+                    Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(adminRole);
+
+                    break;
+                case "mod":
+                    Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(modRole);
+
+                    break;
+                default:
+                    Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(userRole);
+            }
+        });
+    }
+		user.setRoles(roles);
+		userRepository.save(user);
+		return ResponseEntity.ok("User registered successfully!");
+    }
+
+    @PostMapping("/api/auth/connexion")
+    public ResponseEntity<?> connexion(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(
+                            jwt,
+                            userDetails.getId(),
+                            userDetails.getUsername(),
+                            userDetails.getEmail(),
+                            userDetails.getNom(),
+                            userDetails.getPrenom(),
+                            userDetails.getCivilite(),
+                            roles,
+                            userDetails.getDerniereConnexion(),
+                            userDetails.getDateInscription()));
+    }
+}
