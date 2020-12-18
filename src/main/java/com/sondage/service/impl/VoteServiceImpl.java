0d50e00e35e744b192c8642e23a2a96668acadcb
Imane75 <taabdante@et.esiea.fr>
2020-12-18 22:22:26 +0100

    implémentation des services

--- src/main/java/com/sondage/service/impl/VoteServiceImpl.java
+++ src/main/java/com/sondage/service/impl/VoteServiceImpl.java
@@ -1,4 +1,51 @@
 package com.sondage.service.impl;
 
-public class VoteServiceImpl {
+import com.sondage.DAO.impl.UserDetailsImpl;
+import com.sondage.model.*;
+import com.sondage.model.repository.*;
+import com.sondage.service.interfaces.VoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.List;
+import java.util.Optional;
+
+public class VoteServiceImpl implements VoteService {
+    @Autowired
+    private VoteRepository voteRepository;
+    @Autowired
+    private SondageRepository sondageRepository;
+    @Autowired
+    private DatesRepository dateRepository;
+    @Autowired
+    private LieuRepository lieuRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Vote voter(Vote vote) {
+        UserDetailsImpl connectedUser =  (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Sondage sondage = this.sondageRepository.findById(vote.getSondage().getId()).get();
+        Lieu lieu = this.lieuRepository.findById(vote.getLieu().getId()).get();
+        Dates date = this.dateRepository.findById(vote.getDate().getId()).get();
+        User votant = this.userRepository.findById(connectedUser.getId()).get();
+
+        Vote voter = new Vote();
+        voter.setSondage(sondage);
+        voter.setVotant(votant);
+        voter.setLieu(lieu);
+        voter.setDate(date);
+
+        return this.voteRepository.save(vote);
+    }
+
+    @Override
+    public List<Optional<Vote>> getMyVotes(User user) {
+        return this.voteRepository.findByVotant(user);
+    }
+
+    public void deleteVote(Long voteId) {
+        this.voteRepository.deleteById(voteId);
+    }
 }
--- src/main/java/com/sondage/service/interfaces/VoteService.java
+++ src/main/java/com/sondage/service/interfaces/VoteService.java
@@ -1,4 +1,13 @@
 package com.sondage.service.interfaces;
 
+import com.sondage.model.User;
+import com.sondage.model.Vote;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface VoteService {
+    Vote voter(Vote vote);
+    List<Optional<Vote>> getMyVotes(User user);
+    void deleteVote(Long voteId);
 }
